32fee46db95c3a0d1efff85de212d96a2a17068c
Mike Elliott <44234262+mikeelliottuk@users.noreply.github.com>
2020-11-17 12:23:33 +0000

    Add files via upload

---
+++ MMA_modify_workspace.ps1
@@ -0,0 +1,43 @@
+﻿#Powershell script to add or remove a Log Analytics workspace to the Microsoft Monitoring Agent
+#Usage - MMA-modify-workspace -action (list/add/remove) -workspaceID -workspaceKey
+
+#initialise the script
+param(
+    [string] $action = "list",
+    [string] $workspaceID = "",
+    [string] $workspaceKey = ""
+)
+$mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
+$usage = "Usage - MMA-modify-workspace -action (list/add/remove) -workspaceID -workspaceKey’"
+
+Switch ($action) #choose code based on the script action selected - default is list
+{
+ list {
+    $currentWorkspaces = $mma.GetCloudWorkspaces()
+    if ($currentWorkspaces.length -ne 0 ){
+        $currentWorkspaces
+        }
+    else {
+        Write-Output 'No workspaces currently configured.'
+    }
+ }
+ add { 
+    
+    #test for arguments workspaceID and workspaceKey
+    if (($workspaceID -ne "") -and ($workspaceKey -ne "")){
+        $mma.AddCloudWorkspace($workspaceId, $workspaceKey)
+        $mma.ReloadConfiguration()
+        Write-Output "New workspace $workspaceID added."
+    }
+    else {
+        Write-Output $usage
+    }
+
+ }
+ remove {
+    $mma.RemoveCloudWorkspace($workspaceId)
+    $mma.ReloadConfiguration()
+    Write-Output "Workspace $workspaceID removed."
+ }
+ Default { $usage }
+}
