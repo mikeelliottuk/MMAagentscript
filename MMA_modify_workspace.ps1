b538cfada393c7cdfdce0d683a1fb457dd7f0d42
Mike Elliott <44234262+mikeelliottuk@users.noreply.github.com>
2020-11-18 16:29:04 +0000

    Update MMA_modify_workspace.ps1
    Added error checking and test for monitoring agent service on target device

--- MMA_modify_workspace.ps1
+++ MMA_modify_workspace.ps1
@@ -1,7 +1,7 @@
 ﻿#Powershell script to add or remove a Log Analytics workspace to the Microsoft Monitoring Agent
 #Usage - MMA-modify-workspace -action (list/add/remove) -workspaceID -workspaceKey
 #Mike Elliott - Nov 2020
-#ver 2020_11_18_001
+#ver 2020_11_18_003
 
 #initialise the script
 param(
@@ -9,37 +9,73 @@ param(
     [string] $workspaceID = "",
     [string] $workspaceKey = ""
 )
+
+#check if the MMA agent service exists, exit the script if not
+try {
+$service=Get-Service 'HealthService' -ErrorAction Stop
+}
+catch
+{
+     Write-Output  "Error - $PSItem"
+     Return 1
+}
+
 $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-$usage = "Usage - MMA-modify-workspace -action (list/add/remove) -workspaceID -workspaceKey’"
+$usage = 'Syntax - MMA-modify-workspace [-action list/add/remove] [-workspaceID <String[]>] [-workspaceKey <String[]>]'
 
 Switch ($action) #choose code based on the script action selected - default is list
 {
  list {
-    $currentWorkspaces = $mma.GetCloudWorkspaces()
-    if ($currentWorkspaces.length -ne 0 ){
-        $currentWorkspaces
+    try {
+        $currentWorkspaces = $mma.GetCloudWorkspaces() 
+        if ($currentWorkspaces.length -ne 0 ){
+            $currentWorkspaces
         }
-    else {
-        Write-Output 'No workspaces currently configured.'
+        else {
+            Write-Output 'No workspaces currently configured.'
+        }
+        return 0
+    }
+    catch{ #write the error to the console
+        Write-Output $PSItem.ToString()
+        return 1
     }
  }
  add { 
-    
-    #test for arguments workspaceID and workspaceKey
-    if (($workspaceID -ne "") -and ($workspaceKey -ne "")){
-        $mma.AddCloudWorkspace($workspaceId, $workspaceKey)
-        $mma.ReloadConfiguration()
-        Write-Output "New workspace $workspaceID added."
+    try {
+        #test for arguments workspaceID and workspaceKey
+        if (($workspaceID -ne "") -and ($workspaceKey -ne "")){
+            $mma.AddCloudWorkspace($workspaceId, $workspaceKey)
+            $mma.ReloadConfiguration()
+            Write-Output "New workspace $workspaceID added."
+        }
+        else {
+            Write-Output $usage
+        }
+        return 0
     }
-    else {
-        Write-Output $usage
+    catch{ #write the error to the console
+        Write-Output $PSItem.ToString()
+        return 1
     }
-
  }
  remove {
-    $mma.RemoveCloudWorkspace($workspaceId)
-    $mma.ReloadConfiguration()
-    Write-Output "Workspace $workspaceID removed."
+    try {
+        #test for arguments workspaceID
+        if ($workspaceID -ne ""){
+            $mma.RemoveCloudWorkspace($workspaceId)
+            $mma.ReloadConfiguration()
+            Write-Output "Workspace $workspaceID removed."
+        }
+        else {
+            Write-Output $usage
+        }
+        return 0
+    }
+    catch { #write the error to the console
+        Write-Output $PSItem.ToString()
+        return 1
+    }
  }
  Default { $usage }
 }
